c145c1d52ef92d5c1a4619a8c48a6d7f03f52431
Almas <seitalmas1982@gmail.com>
2023-01-12 17:52:39 +0600

    ViewModelFactory

--- app/src/main/java/com/almseit/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/almseit/composition/presentation/GameFinishedFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.almseit.composition.R
 import com.almseit.composition.databinding.FragmentGameFinishedBinding
 import com.almseit.composition.domain.entity.GameResult
 
@@ -30,6 +31,12 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupClickListener()
+        bindViews()
+
+    }
+
+    private fun setupClickListener(){
         val callback = object : OnBackPressedCallback(true){
             override fun handleOnBackPressed() {
                 retryGame()
@@ -41,10 +48,43 @@ class GameFinishedFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        parseArgs()
+    private fun bindViews(){
+        binding.emojiResult.setImageResource(getSmileResId())
+        binding.tvRequiredAnswers.text = String.format(
+            getString(R.string.required_score),gameResult.gameSettings.minCountOfRightAnswers
+        )
+        binding.tvScoreAnswers.text = String.format(
+            getString(R.string.score_answers),gameResult.countOfRightAnswers
+        )
+        binding.tvRequiredPercentage.text = String.format(
+            getString(R.string.required_percentage),gameResult.gameSettings.minPercentOfRightAnswers
+        )
+        binding.tvScorePercentage.text = String.format(
+            getString(R.string.score_percentage),getPercentOfRightAnswers()
+        )
     }
+
+    private fun getSmileResId(): Int{
+        return if (gameResult.winner){
+            R.drawable.ic_smile
+        }else{
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+           0
+        } else {
+            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+        }
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            parseArgs()
+        }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/almseit/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/almseit/composition/presentation/GameFragment.kt
@@ -18,9 +18,12 @@ import com.almseit.composition.domain.entity.Level
 class GameFragment : Fragment() {
 
     private lateinit var level: Level
+
+    private  val viewModelFactory by lazy {
+        GameViewModelFactory(requireActivity().application,level)
+    }
     private val viewModel by lazy{
-        ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
-        )[GameViewModel::class.java]
+        ViewModelProvider(this,viewModelFactory)[GameViewModel::class.java]
     }
 
     private val tvOptions by lazy{
@@ -57,7 +60,7 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
+
 
 
     }
